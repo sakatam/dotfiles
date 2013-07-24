bf158743c801b7e384a8186d93488ea4a99ba64d
Makoto Sakata <maco.sakata@gmail.com>
2013-07-24 16:47:29 +0900

    introduced misc configs + bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+# linking
+
+ln -sf $DOTFILES_DIR/ackrc $HOME/.ackrc
+ln -sf $DOTFILES_DIR/inputrc $HOME/.inputrc
+ln -sf $DOTFILES_DIR/screenrc $HOME/.screenrc
+ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
+
+ln -sf $DOTFILES_DIR/git/gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_DIR/git/gitconfig-boxen $HOME/.gitconfig-boxen
---
+++ vimrc
@@ -0,0 +1,18 @@
+scriptencoding utf-8
+
+syntax on
+colorscheme jellybeans
+
+set ignorecase
+set smartcase
+set showmatch
+set autoindent
+set expandtab
+
+" Indicate invisibles
+set listchars=eol:$,trail:_,tab:>\
+
+" Encoding settings
+set fenc=utf-8
+set fencs=iso-2022-jp,euc-jp,cp932,utf-8
+set enc=utf-8
