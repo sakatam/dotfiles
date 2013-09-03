13b179db7507636f0d5fcea8f807e00d44a61cb6
Makoto Sakata <maco.sakata@gmail.com>
2013-09-03 23:17:03 +0900

    added editrc for readline-like keyboard shortcut in mysql
    see http://bugs.mysql.com/bug.php?id=68449

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 # linking
 
 ln -sf $DOTFILES_DIR/ackrc $HOME/.ackrc
+ln -sf $DOTFILES_DIR/editrc $HOME/.editrc
 ln -sf $DOTFILES_DIR/inputrc $HOME/.inputrc
 ln -sf $DOTFILES_DIR/screenrc $HOME/.screenrc
 ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
