9bc959a825727d0b9a8c7d13bab453e0380383fa
Makoto Sakata <maco.sakata@gmail.com>
2014-06-01 21:12:53 +0900

    add gemrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ ln -sf $DOTFILES_DIR/editrc $HOME/.editrc
 ln -sf $DOTFILES_DIR/inputrc $HOME/.inputrc
 ln -sf $DOTFILES_DIR/screenrc $HOME/.screenrc
 ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
+ln -sf $DOTFILES_DIR/gemrc $HOME/.gemrc
 
 ln -sf $DOTFILES_DIR/git/gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_DIR/git/gitconfig-boxen $HOME/.gitconfig-boxen
-
